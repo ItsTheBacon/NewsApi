ce6c8c05181dc7e6719bb3c0a6c33283fa634524
nurjanovb <toxiccaalien00@gmail.com>
2021-10-13 17:36:48 +0600

    design test commit

--- app/src/main/java/com/example/newsapp/ui/adapters/TopHeadLinesAdapter.kt
+++ app/src/main/java/com/example/newsapp/ui/adapters/TopHeadLinesAdapter.kt
@@ -19,8 +19,6 @@ import com.example.newsapp.utils.dateFormat
 class TopHeadLinesAdapter :
     PagingDataAdapter<Everything, TopHeadLinesAdapter.ViewHolder>(BaseDiffUtilCallback<Everything>()) {
     private lateinit var binding: ItemEverythingBinding
-
-
     inner class ViewHolder(private val binding: ItemEverythingBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: Everything) {
